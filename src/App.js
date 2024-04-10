581172d90364686261a987bd166aeb155b1c704d
Jay Pratap <61382653+jaypratap963@users.noreply.github.com>
2024-04-10 18:41:57 +0530

    Neuron

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import FirstComponent from "./firstcomponent";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <FirstComponent />
+    </div>
+  );
+}
+
+export default App;
